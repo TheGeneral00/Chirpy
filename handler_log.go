254bbeda37b6ba594502892194e2bc018d27c233
TheGeneral00 <richard.beldner@outlook.de>
2025-05-07 20:36:06 +0200

    devided functions into handlers to clean up main file. Added profanity filter for chirps

---
+++ handler_log.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+
+func initializeLogger() (*os.File, error) {
+        logFile, err := os.OpenFile("server_log.txt", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+        if err != nil {
+                return nil, fmt.Errorf("Failed to open log file: %v", err)
+        }
+
+        //Send logs to both terminal and file
+        multiWriter := io.MultiWriter(os.Stdout, logFile)
+        log.SetOutput(multiWriter)
+        log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+
+        log.Printf("Loging system initialized")
+
+        return logFile, nil
+}
--- main.go
+++ main.go
@@ -2,81 +2,49 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
 	"sync/atomic"
 )
 
-type apiHandler struct {}
-
-func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
-
 type apiConfig struct {
-        fileServerHits atomic.Int32
-}
-
-func (cfg *apiConfig) middleWareMetricsInc(next http.Handler) http.Handler {
-        return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request){
-                cfg.fileServerHits.Add(1)
-                next.ServeHTTP(w, req)
-        })
+	fileserverHits atomic.Int32
 }
 
 func main() {
-    // Create log file
-    logFile, err := os.OpenFile("server_log.txt", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to open log file: %v\n", err)
-        os.Exit(1)
-    }
-    defer logFile.Close()
+        //Starting up log 
+        logFile, err := initializeLogger()
+        if err != nil {
+                fmt.Fprintf(os.Stderr, "%v\n", err)
+                os.Exit(1)
+        }
+        defer logFile.Close()
 
-    // Send logs to both terminal and file
-    multiWriter := io.MultiWriter(os.Stdout, logFile)
-    log.SetOutput(multiWriter)
-    log.SetFlags(log.LstdFlags | log.Lmicroseconds)
-    
-    log.Println("Logging system initialized")
 
-    //initialize Config to track metrics 
-    var apiCfg apiConfig
-    
-    // Create and start server
-    mux := http.NewServeMux()
-    server := &http.Server{
-        Addr:    ":8080",
-        Handler: mux,
-    }
-    //adding http.FileServer as handler with root /content
-    handler := http.FileServer(http.Dir("."))
-    mux.Handle("/app/", apiCfg.middleWareMetricsInc(handler))
+	const filepathRoot = "./app"
+	const port = "8080"
 
-    //adding a readiness endpoint
-    mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, req *http.Request) {
-            w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-            w.WriteHeader(200)
-            w.Write([]byte("OK\n"))
-    })
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
-    // adding metrics endpoint
-    mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, req *http.Request) {
-            w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-            w.WriteHeader(200)
-            hitCount := apiCfg.fileServerHits.Load()
-            string := fmt.Sprintf("Hits: %v\n", hitCount)
-            w.Write([]byte(string))
-    })
+	mux := http.NewServeMux()
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", fsHandler)
 
-    mux.HandleFunc("POST /reset", func(w http.ResponseWriter, req *http.Request) {
-            apiCfg.fileServerHits.Store(0) 
-            w.Write([]byte("Metrics have been resetted.\n"))
-    })
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
 
-    log.Printf("Starting server on port %v\n", server.Addr)
-    err = server.ListenAndServe()
-    if err != nil {
-        log.Fatal("Server error:", err)
-    }
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
+
